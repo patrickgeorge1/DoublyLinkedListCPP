5955d2902cf29c1d7727a523bc7674a3a4c331ab
Patrick <=>
2019-03-10 15:58:12 +0200

    added clear, isEmpty, insert_index, delete_index, getHead, getTail, = and I have to fix +

--- Makefile
+++ Makefile
@@ -1,3 +1,13 @@
 build:
 	g++ doubly-linked.cpp -o a.out
 	./a.out
+
+valgrind:
+	g++ doubly-linked.cpp -o a.out
+	valgrind --leak-check=yes ./a.out
+
+debug:
+	g++ -g doubly-linked.cpp -o a.out
+
+remove:
+	rm a.out
--- doubly-linked.cpp
+++ doubly-linked.cpp
@@ -120,8 +120,136 @@ public:
 		return x;
 	}
 
+	int insert_index(int index, var data) {
+		if(size == 0) {
+			push_back(data);
+			return 1;
+		} 
+		if(index > size) {
+			push_back(data);
+			return 1;
+		} 
+		if(index == 0 || index == 1) {
+			push_front(data);
+			return 1;
+		} 
+		if(index < 0)
+		{
+			 	index = ((-1)*index)%size;
+			 	if (index == 1) 
+			 	{
+			 		push_back(data);
+			 		return 1;
 
+			 	}
+			 	else if (index == 0)
+					 	{
+							push_front(data);
+							return 1; 
+					 	}
+					 	else
+					 	 {
+					 		Node<var> *now = tail;
+						 	for(int i = 1; i <= index-2; i++)
+						 		now = now->prev;
+						 	Node<var> *added = new Node<var>(data);
+						 	added->next = now;
+						 	added->prev = now->prev;
+						 	now->prev->next = added;
+						 	now->prev = added;
 
+						 	size++;
+						 	return 1;
+					 	}
+	
+
+		}
+		if(index > 0) 
+		{
+			Node<var> *now = head;
+		 	for(int i = 1; i <= index-2; i++)
+		 		now = now->next;
+		 	Node<var> *added = new Node<var>(data);
+		 	added->prev = now;
+		 	added->next = now->next;
+		 	now->next->prev = added;
+		 	now->next = added;
+		 	size++;
+		 	return 1; 
+		}
+		return 0;
+
+	}
+
+	int delete_index(int index) {
+		if(size == 0) {
+			pop_back();
+			return 1;
+		} 
+		if(index > size) {
+			pop_back();
+			return 1;
+		} 
+		if(index == 0 || index == 1) {
+			pop_front();
+			return 1;
+		}
+		if(index == size) {
+			pop_back();
+			return 1;
+		} 
+		if(index < 0)
+		{
+			 	index = ((-1)*index)%size;
+			 	if (index == 1) 
+			 	{
+			 		pop_back();
+			 		return 1;
+
+			 	}
+			 	else if (index == 0)
+					 	{
+							pop_front();
+							return 1; 
+					 	}
+					 	else
+					 	 {
+					 		Node<var> *now = tail;
+						 	for(int i = 1; i <= index-1; i++)
+						 		now = now->prev;
+						 	Node<var> *deleted = now;
+						 	deleted->prev->next = deleted->next;
+						 	deleted->next->prev = deleted->prev;
+						 	size--;
+						 	delete deleted;
+						 	return 1;
+					 	}
+	
+
+		}
+		if(index > 0) 
+		{
+			Node<var> *now = head;
+		 	for(int i = 1; i <= index-1; i++)
+		 		now = now->next;
+		 	Node<var> *deleted = now;
+		 	deleted->prev->next = deleted->next;
+			deleted->next->prev = deleted->prev;
+		 	size--;
+		 	delete deleted;
+		 	return 1; 
+		}
+		return 0;
+
+	}
+
+	int length() {
+		return size;
+	}
+
+	bool isEmpty() {
+		return (size == 0);
+	}
 
 	void print() {											// print head->tail
 		Node<var> *now = head;
@@ -140,17 +268,73 @@ public:
 			now = now->prev;
 		}
 	}
+
+	void clear() {
+		if(size != 0)
+		{
+			Node<var> *now = head;
+			now = now->next;
+			while (now != nullptr)
+			{
+				now->prev->next = nullptr;
+				delete now->prev;
+				now = now->next;
+			}
+			delete tail;
+			tail = nullptr;
+			delete head;
+			head = nullptr;
+		}
+		size = 0;
+	
+	}
+
+	Node<var> * getHead() {
+		return head;
+	}
+
+	Node<var> * getTail() {
+		return head;
+	}
+
+	List<var>& operator=(List<var> const other) {
+		clear();
+		Node<var> *now = other.head;
+		while(now != nullptr) {
+			push_back(now->data);
+			now = now->next;
+		}
+		return *this;
+	}
+
+	List<var>& operator+(List<var> const other) {
+		Node<var> *now = other.head;
+		Node<var> *aux;
+		while(now != nullptr) {
+			push_back(now->data);
+			aux = now;
+ 			now = now->next;
+		}
+		tail = aux;
+		return *this;
+	}
+
 };
 
 int main() {
 
-	List<int> x;
+	List<int> x, y;
 	x.push_back(1);
 	x.push_back(2);
+	x.push_back(3);
 	x.push_back(4);
-	x.push_front(0);
-	x.pop_back();
-	x.pop_front();
+
+	y.push_back(11);
+	y.push_back(12);
+	y.push_back(13);
+	y.push_back(14);
+
+	x = x+y;
 	x.print();
 
 	return 0;
